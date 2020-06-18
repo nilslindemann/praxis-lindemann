7e800ccb9c2de9a15fd6f651a034cff5d709d456
Nils Lindemann <nilslindemann@tutanota.com>
2020-06-19 01:58:01 +0200

    Geanyfy

--- scripts/autolocate.js
+++ scripts/autolocate.js
@@ -1,19 +1,19 @@
 "use strict";
 
 window.onload = function(){
-	
-	let menulinks = document
-		.getElementById("js-autolocate")
-		.getElementsByTagName("a");
 
-	let filename = location.pathname.substring(
-		location.pathname.lastIndexOf("/") + 1
-	);
+    let menulinks = document
+        .getElementById("js-autolocate")
+        .getElementsByTagName("a");
 
-	for (let link of menulinks) {
-		if (link.getAttribute("href") === filename) {
-			link.classList.add("active");
-			break;
-		}
-	}
+    let filename = location.pathname.substring(
+        location.pathname.lastIndexOf("/") + 1
+    );
+
+    for (let link of menulinks) {
+        if (link.getAttribute("href") === filename) {
+            link.classList.add("active");
+            break;
+        }
+    }
 }
